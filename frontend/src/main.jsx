393d29c4afbf51a6620ab8e0093ec32a6af2b7a5
Anandhu Aniyan <anadhu369@gmail.com>
2026-08-26 23:11:35 +0800

    feat: gate analytics and ads behind consent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
+import Tracking from "./components/Tracking";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(<BrowserRouter><App /></BrowserRouter>);
+createRoot(document.getElementById("root")).render(<BrowserRouter><Tracking/><App /></BrowserRouter>);
